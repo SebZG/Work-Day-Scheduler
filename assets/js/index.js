35edb46258e18aaaeb60a180cb2a40aed12dd3ce
SebZG <sebastian.zapata.g@gmail.com>
2024-01-17 05:26:19 +0000

    Mutliple timeBlocks dynamically created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,28 @@
 // Set current day/time for header
 $("#currentDay").text(`${dayjs().format("dddd, D MMMM YYYY")}`);
 
-const hour = 9;
-const ti = dayjs(`2024 ${hour}:00`).format("hh A");
-
 const plannerEl = $("#planner");
-const timeBlock = $(
-  `
-  <form>
-  <label class="hour col-1" for="entry">${ti}</label>
-  <input class="present col-10" name="entry" />
-  <button type="submit" class="saveBtn col-1"><i class="fa-solid fa-floppy-disk"></i></button>
-  </form>
-  `
-);
 
-plannerEl.append(timeBlock);
+// Create multiple time blocks
+for (let hour = 9; hour <= 17; hour++) {
+  const time = dayjs(`2024 ${hour}:00`).format("hh A");
+
+  const timeBlock = $(`
+    <form data-time="${hour}">
+      <label class="hour col-1" for="entry"><p>${time}</p></label>
+      <input class="present col-10" name="entry" />
+      <button type="submit" class="saveBtn col-1"><i class="fa-solid fa-floppy-disk"></i></button>
+    </form>
+  `);
+
+  plannerEl.append(timeBlock);
+}
 
 const handleSubmit = (e) => {
   e.preventDefault();
   const inputValue = $(e.target).find('input[name="entry"]').val();
-  console.log("Input Value:", inputValue);
-  $("input").val(inputValue)
+  const time = $(e.target).data('time');
+  console.log(`Input Value for time ${time}:`, inputValue);
 }
 
 $("form").on("submit", handleSubmit);
