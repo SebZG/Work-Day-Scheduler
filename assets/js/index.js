032450c3a1b42e16078de44b66463582929e5fe5
SebZG <sebastian.zapata.g@gmail.com>
2024-01-17 06:34:49 +0000

    background-colors updating dynamically

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,27 @@ $("#currentDay").text(`${dayjs().format("dddd, D MMMM YYYY")}`);
 
 const plannerEl = $("#planner");
 
+// Determine time status
+const getTimeStatus = (hour) => {
+  const currentHour = dayjs().hour();
+  if (hour < currentHour) {
+    return "past";
+  } else if (hour === currentHour) {
+    return "present";
+  } else {
+    return "future";
+  }
+};
+
 // Create multiple time blocks
 for (let hour = 9; hour <= 17; hour++) {
   const time = dayjs(`2024 ${hour}:00`).format("hh A");
+  const timeStatus = getTimeStatus(hour);
 
   const timeBlock = $(`
     <form data-time="${hour}">
       <label class="hour col-1" for="entry"><p>${time}</p></label>
-      <input class="col-10" name="entry" />
+      <input class="col-10 ${timeStatus}" name="entry" />
       <button type="submit" class="saveBtn col-1"><i class="fa-solid fa-floppy-disk"></i></button>
     </form>
   `);
@@ -29,7 +42,7 @@ const handleSubmit = (e) => {
 $("form").on("submit", handleSubmit);
 
 // Retrieve stored values
-$(document).ready(function () {
+$(document).ready(() => {
   for (let hour = 9; hour <= 17; hour++) {
     const storedValue = localStorage.getItem(`time_${hour}`);
     if (storedValue) {
