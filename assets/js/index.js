9149a8f166db50f0f06a2fa4241423395681d611
SebZG <sebastian.zapata.g@gmail.com>
2024-01-17 05:42:43 +0000

    Local storage funcionallity added

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,7 +10,7 @@ for (let hour = 9; hour <= 17; hour++) {
   const timeBlock = $(`
     <form data-time="${hour}">
       <label class="hour col-1" for="entry"><p>${time}</p></label>
-      <input class="present col-10" name="entry" />
+      <input class="col-10" name="entry" />
       <button type="submit" class="saveBtn col-1"><i class="fa-solid fa-floppy-disk"></i></button>
     </form>
   `);
@@ -23,6 +23,17 @@ const handleSubmit = (e) => {
   const inputValue = $(e.target).find('input[name="entry"]').val();
   const time = $(e.target).data('time');
   console.log(`Input Value for time ${time}:`, inputValue);
+  localStorage.setItem(`time_${time}`, inputValue);
 }
 
 $("form").on("submit", handleSubmit);
+
+// Retrieve stored values
+$(document).ready(function () {
+  for (let hour = 9; hour <= 17; hour++) {
+    const storedValue = localStorage.getItem(`time_${hour}`);
+    if (storedValue) {
+      $(`form[data-time="${hour}"] input[name="entry"]`).val(storedValue);
+    }
+  }
+});
